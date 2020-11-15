24a00317d0774f8d3488374601abfbb15d4be167
Ruckard Smith <ruckard.smith@gmail.com>
2020-11-15 12:59:33 +0100

    Added leverage support

--- zignaly-signal-wizard/zignaly-signal-wizard.js
+++ zignaly-signal-wizard/zignaly-signal-wizard.js
@@ -2,7 +2,7 @@
 let suggestedSignal = document.getElementById('suggestedSignal');
 let signalButton = document.getElementById('signalWizardButton');
 
-updateInfo();
+zigProviderTypeChange();
 
 function setSuggestedSignal() {
   // Define variables
@@ -26,8 +26,11 @@ function setSuggestedSignal() {
 
   // Side
   var zigSide = document.getElementById("zigSide").value;
+
+  // Leverage
+  var zigLeverage = document.getElementById("zigLeverage").value;
   
-  suggestedSignal.innerHTML = zigExchange + zigExchangeType + zigSide;
+  suggestedSignal.innerHTML = zigExchange + zigExchangeType + zigSide + zigLeverage;
 }
 
 function zigProviderTypeChange() {
@@ -101,6 +104,30 @@ function zigExchangeTypeChange() {
 
       zigSide.add(el);
   }
+
+  var spotLeverages = ["1"];
+  var futuresLeverages = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125"];
+
+  if (zigExchangeType === 'spot') {
+      usedLeverages = spotLeverages;
+  }
+  if (zigExchangeType === 'futures') {
+      usedLeverages = futuresLeverages;
+  }
+
+  var zigLeverage = document.getElementById("zigLeverage");
+  zigLeverage.innerHTML = "";
+
+  for(var i = 0; i < usedLeverages.length; i++) {
+      var opt = usedLeverages[i];
+
+      var el = document.createElement("option");
+      el.value = opt;
+      el.text = el.value + 'x';
+
+      zigLeverage.add(el);
+  }
+
   updateInfo();
 
 }
@@ -146,6 +173,10 @@ function zigExchangeChange() {
 }
 
 function zigSideChange() {
+  updateInfo();
+}
+
+function zigLeverageChange() {
   updateInfo();
 }
 
