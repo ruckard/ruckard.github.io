665af03c48cdec47f4d07f5588d69edae7458ec7
Ruckard Smith <ruckard.smith@gmail.com>
2020-11-25 20:24:49 +0100

    Leverage is now only used from an entry order.

--- zignaly-signal-wizard/zignaly-signal-wizard.js
+++ zignaly-signal-wizard/zignaly-signal-wizard.js
@@ -336,6 +336,7 @@ function zigProviderTypeChange() {
 
 function zigExchangeTypeChange() {
   var zigExchangeType = document.getElementById("zigExchangeType").value;
+  var zigSignalType = document.getElementById("zigSignalType").value;
 
   var spotExchangeTypeSides = ["long"];
   var futuresExchangeTypeSides = ["long", "short"];
@@ -365,13 +366,16 @@ function zigExchangeTypeChange() {
       zigSide.add(el);
   }
 
+  var noneLeverages = [];
   var spotLeverages = ["1"];
   var futuresLeverages = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125"];
 
-  if (zigExchangeType === 'spot') {
+  usedLeverages = noneLeverages;
+
+  if ((zigExchangeType === 'spot') && (zigSignalType === 'entry')) {
       usedLeverages = spotLeverages;
   }
-  if (zigExchangeType === 'futures') {
+  if ((zigExchangeType === 'futures') && (zigSignalType === 'entry')) {
       usedLeverages = futuresLeverages;
   }
 
@@ -510,6 +514,7 @@ function zigSignalTypeChange() {
       zigOrderType.add(el);
   }
   zigOrderTypeChange(); // Force OrderType change to be taken into account
+  zigExchangeTypeChange(); // Force leverage change to be taken into account
   updateInfo();
 }
 
